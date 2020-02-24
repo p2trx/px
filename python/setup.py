a80fec60571c7b451b7478acb5dc84ae44f1e5d8
Anh Le <anhqle@kms-technology.com>
2020-02-24 16:01:46 +0700

    update version

--- python/setup.py
+++ python/setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+import px;
 
 setup(
     name='px',
-    version='0.0.1',
+    version=px.__version__,
     packages=find_packages(),
     install_requires=['grpcio>=1.27.2'],
     author='p2trx',
