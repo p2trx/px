91d547e325e3390e79a9bf061cad69ba6f964f22
sonhuynhpham <sonhuynhpham@kms-technology.com>
2020-02-24 13:51:43 +0700

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='px',
-    version='1.0.0',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=['grpcio', 'grpcio-tools'],
+    author='p2trx',
+    description='Modern test automation framework using GRPC protocol',
+    url='https://github.com/p2trx/px',
 )
