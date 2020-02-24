137d79b675d1090a85ddc23e3a8360f384014342
sonhuynhpham <sonhuynhpham@kms-technology.com>
2020-02-24 14:43:12 +0700

    Remove grpc files and update dependencies in setup.py

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ setup(
     name='px',
     version='0.0.1',
     packages=find_packages(),
-    install_requires=['grpcio', 'grpcio-tools'],
+    install_requires=['grpcio>=1.27.2'],
     author='p2trx',
     description='Modern test automation framework using GRPC protocol',
     url='https://github.com/p2trx/px',
