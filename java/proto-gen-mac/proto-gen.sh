af0e8625940b6dbf4ce6884992d89acb7d350a03
Qui Le <quile@kms-technology.com>
2020-02-27 11:45:35 +0700

    update

--- java/proto-gen-mac/proto-gen.sh
+++
@@ -1,4 +0,0 @@
-OUTPUT_DIR=../src/main/java
-PROTO_DIR=../proto/*.proto
-mkdir -p ${OUTPUT_DIR}
-./protoc --plugin=protoc-gen-grpc-java=protoc-gen-grpc-java.exe -I=../ --java_out=${OUTPUT_DIR} --grpc-java_out=${OUTPUT_DIR} ${PROTO_DIR}
