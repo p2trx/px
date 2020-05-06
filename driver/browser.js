a5e40d98ef05b20455dabb26856d384bf850de51
Qui Le <quile@kms-technology.com>
2020-05-06 08:18:46 +0700

    update

--- driver/browser.js
+++ driver/browser.js
@@ -1,11 +1,25 @@
 const puppeteer = require('puppeteer')
+const path = require('path')
 
 const launch = async headless => {
-  global.browser = await puppeteer.launch({ headless })
+  const executablePath = getChromiumExecutablePath();
+  global.browser = await puppeteer.launch({ headless, executablePath })
   const { browser } = global
   global.page = await browser.newPage()
   const { page } = global
   return page.setViewport({ width: 1024, height: 768 })
+}
+
+const getChromiumExecutablePath = () => {
+  let chromiumExecutablePath = puppeteer.executablePath();
+  const isPkg = typeof process.pkg !== 'undefined'
+  if (isPkg) {
+    chromiumExecutablePath = puppeteer.executablePath().replace(
+      /^.*?\/node_modules\/puppeteer\/\.local-chromium/,
+      path.join(path.dirname(process.execPath), 'chromium')
+    )
+  }
+  return chromiumExecutablePath;
 }
 
 const close = () => {
