5a1cf543c0ddd944e036ca965c125b50202e65a2
Qui Le <quile@kms-technology.com>
2020-05-06 17:10:07 +0700

    update

--- python/client.py
+++ python/client.py
@@ -3,48 +3,42 @@ import px.px_pb2 as px
 from px.px_pb2_grpc import BrowserStub
 from timeit import default_timer as timer
 
+def click(selector):
+    px.Action(clickAction=px.ClickAction(selector=selector))
+
+def select(selector, values=[]):
+    px.Action(selectAction=px.SelectAction(selector=selector, values=values))
+
+def type(selector, text):
+    px.Action(typeAction=px.TypeAction(selector=selector, text=text)),
+
+def get_text(selector):
+    px.Action(getInnerTextAction=px.GetInnerTextAction(selector=selector)),
 
 def doActions(stub):
     start = timer()
     actions = [
-        px.Action(typeAction=px.TypeAction(
-            selector='#first-name', text='First Name')),
-        px.Action(typeAction=px.TypeAction(
-            selector='#last-name', text='Last Name')),
-        px.Action(clickAction=px.ClickAction(
-            selector='input[type=radio][name=gender]')),
-        px.Action(clickAction=px.ClickAction(selector='#dob')),
-        px.Action(clickAction=px.ClickAction(
-            selector='body > div.datepicker.datepicker-dropdown.dropdown-menu.datepicker-orient-left.datepicker-orient-top > div.datepicker-days > table > tbody > tr:nth-child(1) > td:nth-child(1)')),
-        px.Action(typeAction=px.TypeAction(
-            selector='#address', text='Address')),
-        px.Action(typeAction=px.TypeAction(
-            selector='#email', text='email@email.com')),
-        px.Action(typeAction=px.TypeAction(
-            selector='#password', text='Password')),
-        px.Action(typeAction=px.TypeAction(
-            selector='#company', text='Company')),
-        px.Action(selectAction=px.SelectAction(
-            selector='#role', values=['Manager'])),
-        px.Action(selectAction=px.SelectAction(
-            selector='#expectation', values=['High salary'])),
-        px.Action(clickAction=px.ClickAction(
-            selector='.development-ways .checkbox:nth-child(1) input')),
-        px.Action(clickAction=px.ClickAction(
-            selector='.development-ways .checkbox:nth-child(2) input')),
-        px.Action(clickAction=px.ClickAction(
-            selector='.development-ways .checkbox:nth-child(3) input')),
-        px.Action(clickAction=px.ClickAction(
-            selector='.development-ways .checkbox:nth-child(4) input')),
-        px.Action(clickAction=px.ClickAction(
-            selector='.development-ways .checkbox:nth-child(5) input')),
-        px.Action(clickAction=px.ClickAction(
-            selector='.development-ways .checkbox:nth-child(6) input')),
-        px.Action(typeAction=px.TypeAction(
-            selector='#comment', text='Comment')),
-        px.Action(clickAction=px.ClickAction(selector='#submit')),
-        px.Action(getInnerTextAction=px.GetInnerTextAction(
-            selector='#submit-msg')),
+        type(selector='#first-name', text='First Name'),
+        type(selector='#first-name', text='First Name'),
+        type(selector='#last-name', text='Last Name'),
+        click(selector='input[type=radio][name=gender]'),
+        click(selector='#dob'),
+        click(selector='body > div.datepicker.datepicker-dropdown.dropdown-menu.datepicker-orient-left.datepicker-orient-top > div.datepicker-days > table > tbody > tr:nth-child(1) > td:nth-child(1)'),
+        type(selector='#address', text='Address'),
+        type(selector='#email', text='email@email.com'),
+        type(selector='#password', text='Password'),
+        type(selector='#company', text='Company'),
+        select(selector='#role', values=['Manager']),
+        select(selector='#expectation', values=['High salary']),
+        click(selector='.development-ways .checkbox:nth-child(1) input'),
+        click(selector='.development-ways .checkbox:nth-child(2) input'),
+        click(selector='.development-ways .checkbox:nth-child(3) input'),
+        click(selector='.development-ways .checkbox:nth-child(4) input'),
+        click(selector='.development-ways .checkbox:nth-child(5) input'),
+        click(selector='.development-ways .checkbox:nth-child(6) input'),
+        type(selector='#comment', text='Comment'),
+        click(selector='#submit'),
+        get_text(selector='#submit-msg'),
     ]
     request = px.DoRequest(actions=actions)
     stub.Do(request)
