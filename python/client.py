67c1b2a4a8459216ed8d67b792b4c592784dc077
Qui Le <quile@kms-technology.com>
2020-05-08 14:50:06 +0700

    update

--- python/client.py
+++ python/client.py
@@ -1,34 +1,51 @@
+import grpc
 import px.px_pb2 as px
+from px.px_pb2_grpc import BrowserStub
 
-def launch(stub):
+latest_stub = None
+
+def launch(stub=None):
     action = px.Action(launchAction=px.LaunchAction(headless=False))
     do_request(stub=stub, actions=[action])
 
 
-def goto(stub, url):
+def goto(url, stub=None):
     actions = px.Action(gotoAction=px.GotoAction(url=url))
     do_request(stub=stub, actions=[actions])
 
 
-def type(stub, selector, text):
+def type(selector, text, stub=None):
     actions = px.Action(typeAction=px.TypeAction(selector=selector, text=text))
     do_request(stub=stub, actions=[actions])
 
 
-def click(stub, selector):
+def click(selector, stub=None):
     actions = px.Action(clickAction=px.ClickAction(selector=selector))
     do_request(stub=stub, actions=[actions])
 
 
-def select(stub, selector, values):
+def select(selector, values, stub=None):
     actions = px.Action(selectAction=px.SelectAction(selector=selector, values=values))
     do_request(stub=stub, actions=[actions])
 
 
-def get_text(stub, selector):
+def get_text(selector, stub=None):
     actions = px.Action(getInnerTextAction=px.GetInnerTextAction(selector=selector))
     do_request(stub=stub, actions=[actions])
 
-def do_request(stub, actions):
+def do_request(actions, stub=None):
+    global latest_stub
+    if stub is None:
+        stub = latest_stub
     request = px.DoRequest(actions=actions)
     stub.Do(request)
+
+def setup(url):
+    global latest_stub
+    channel = grpc.insecure_channel(url)
+    stub = BrowserStub(channel)
+    latest_stub = stub
+    return stub
+    # with grpc.insecure_channel(url) as channel:
+    #     latest_stub = BrowserStub(channel)
+    #     return latest_stub
--- python/test.py
+++ python/test.py
@@ -1,45 +1,45 @@
-import grpc
-from px.px_pb2_grpc import BrowserStub
 from timeit import default_timer as timer
 import client as px
 
 
-def doActions(stub):
+def doActions():
     start = timer()
-    px.type(stub=stub, selector='#first-name', text='First Name'),
-    px.type(stub=stub, selector='#last-name', text='Last Name'),
-    px.click(stub=stub, selector='input[type=radio][name=gender]'),
-    px.click(stub=stub, selector='#dob'),
-    px.click(stub=stub, selector='body > div.datepicker.datepicker-dropdown.dropdown-menu.datepicker-orient-left.datepicker-orient-top > div.datepicker-days > table > tbody > tr:nth-child(1) > td:nth-child(1)'),
-    px.type(stub=stub, selector='#address', text='Address'),
-    px.type(stub=stub, selector='#email', text='email@email.com'),
-    px.type(stub=stub, selector='#password', text='Password'),
-    px.type(stub=stub, selector='#company', text='Company'),
-    px.select(stub=stub, selector='#role', values=['Manager']),
-    px.select(stub=stub, selector='#expectation', values=['High salary']),
-    px.click(stub=stub, selector='.development-ways .checkbox:nth-child(1) input'),
-    px.click(stub=stub, selector='.development-ways .checkbox:nth-child(2) input'),
-    px.click(stub=stub, selector='.development-ways .checkbox:nth-child(3) input'),
-    px.click(stub=stub, selector='.development-ways .checkbox:nth-child(4) input'),
-    px.click(stub=stub, selector='.development-ways .checkbox:nth-child(5) input'),
-    px.click(stub=stub, selector='.development-ways .checkbox:nth-child(6) input'),
-    px.type(stub=stub, selector='#comment', text='Comment'),
-    px.click(stub=stub, selector='#submit'),
-    px.get_text(stub=stub, selector='#submit-msg'),
+    px.type(selector='#first-name', text='First Name'),
+    px.type(selector='#last-name', text='Last Name'),
+    px.click(selector='input[type=radio][name=gender]'),
+    px.click(selector='#dob'),
+    px.click(selector='body > div.datepicker.datepicker-dropdown.dropdown-menu.datepicker-orient-left.datepicker-orient-top > div.datepicker-days > table > tbody > tr:nth-child(1) > td:nth-child(1)'),
+    px.type(selector='#address', text='Address'),
+    px.type(selector='#email', text='email@email.com'),
+    px.type(selector='#password', text='Password'),
+    px.type(selector='#company', text='Company'),
+    px.select(selector='#role', values=['Manager']),
+    px.select(selector='#expectation', values=['High salary']),
+    px.click(selector='.development-ways .checkbox:nth-child(1) input'),
+    px.click(selector='.development-ways .checkbox:nth-child(2) input'),
+    px.click(selector='.development-ways .checkbox:nth-child(3) input'),
+    px.click(selector='.development-ways .checkbox:nth-child(4) input'),
+    px.click(selector='.development-ways .checkbox:nth-child(5) input'),
+    px.click(selector='.development-ways .checkbox:nth-child(6) input'),
+    px.type(selector='#comment', text='Comment'),
+    px.click(selector='#submit'),
+    px.get_text(selector='#submit-msg'),
     elapsed = timer() - start
     print('Elapsed: {:.3f}s'.format(elapsed))
 
 
-def do(stub):
-    px.launch(stub=stub),
-    px.goto(stub=stub, url='https://katalon-test.s3.amazonaws.com/aut/html/form.html')
-    doActions(stub)
+def do():
+    px.launch(),
+    px.goto(url='https://katalon-test.s3.amazonaws.com/aut/html/form.html')
+    doActions()
 
 
 def run():
-    with grpc.insecure_channel('localhost:50000') as channel:
-        stub = BrowserStub(channel)
-        do(stub)
+    px.setup(url='localhost:50000')
+    do()
+    # with grpc.insecure_channel('localhost:50000') as channel:
+    #     stub = BrowserStub(channel)
+    #     do(stub)
 
 
 if __name__ == "__main__":
