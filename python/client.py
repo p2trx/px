e901e74360b819d218e9665bdf616a37c1099365
Qui Le <quile@kms-technology.com>
2020-05-11 15:04:16 +0700

    update

--- python/client.py
+++ python/client.py
@@ -21,33 +21,33 @@ class Client:
 
     def launch(self):
         action = px.Action(launchAction=px.LaunchAction(headless=False))
-        self.do_request(actions=[action])
+        return self.do_request(actions=[action])
 
 
     def goto(self, url):
         actions = px.Action(gotoAction=px.GotoAction(url=url))
-        self.do_request(actions=[actions])
+        return self.do_request(actions=[actions])
 
 
     def type(self, selector, text):
         actions = px.Action(typeAction=px.TypeAction(selector=selector, text=text))
-        self.do_request(actions=[actions])
+        return self.do_request(actions=[actions])
 
 
     def click(self, selector):
         actions = px.Action(clickAction=px.ClickAction(selector=selector))
-        self.do_request(actions=[actions])
+        return self.do_request(actions=[actions])
 
 
     def select(self, selector, values):
         actions = px.Action(selectAction=px.SelectAction(selector=selector, values=values))
-        self.do_request(actions=[actions])
+        return self.do_request(actions=[actions])
 
 
     def get_inner_text(self, selector):
         actions = px.Action(getInnerTextAction=px.GetInnerTextAction(selector=selector))
-        self.do_request(actions=[actions])
+        return self.do_request(actions=[actions])
 
     def do_request(self, actions):
         request = px.DoRequest(actions=actions)
-        self.stub.Do(request)
+        return self.stub.Do(request)
