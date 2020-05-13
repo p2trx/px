feac47bb4ebc69f31d2009b6325b044f230e103c
Qui Le <quile@kms-technology.com>
2020-05-13 09:47:38 +0700

    update

--- python/server.py
+++ python/server.py
@@ -1,6 +1,6 @@
 import urllib.request
 import zipfile
-import os, stat, errno, subprocess, socket
+import os, stat, errno, subprocess, socket, platform
 import threading
 from pathlib import Path
 
@@ -8,37 +8,48 @@ class Server:
 
     px_home_dir = os.path.join(Path.home(), '.px')
 
-    px_server_package_download_url = 'https://datquach.s3.amazonaws.com/px.zip'
+    windows_px_server_package_download_url = 'https://datquach.s3.amazonaws.com/px-windows.zip'
 
-    unzip_px_server_package_path = os.path.join(px_home_dir, 'px-server')
+    mac_px_server_package_download_url = 'https://datquach.s3.amazonaws.com/px-macos.zip'
 
-    px_server_executable_file_path = os.path.join(unzip_px_server_package_path, 'px')
+    linux_px_server_package_download_url = 'https://datquach.s3.amazonaws.com/px-linux.zip'
 
-    grpc_file_path = os.path.join(unzip_px_server_package_path, 'grpc_node.node')
+    px_server_package_path = os.path.join(px_home_dir, 'px-server')
 
     port = None
 
     thread = None
+
+    os_name = platform.system()
 
     def __init__(self):
         self.setup()
         self.start()
 
     def setup(self):
-        px_server_package_download_url = self.px_server_package_download_url
-        unzip_px_server_package_path = self.unzip_px_server_package_path
-        px_server_executable_file_path = self.px_server_executable_file_path
-        grpc_file_path = self.grpc_file_path
+        self.download_and_extract_px_server_package()
+
+
+    def download_and_extract_px_server_package(self, os_name=None, version=None, px_server_package_path=None):
+        if os_name is None:
+            os_name = self.os_name
+
+        if px_server_package_path is None:
+            px_server_package_path = self.px_server_package_path
+
+        px_server_executable_file_path = os.path.join(px_server_package_path, 'px')
+        grpc_file_path = os.path.join(px_server_package_path, 'grpc_node.node')
+        px_server_package_download_url = self.get_px_server_package_download_url(os_name=os_name, version=version)
 
         if not os.path.exists(px_server_executable_file_path) or not os.path.exists(grpc_file_path):
             try:
-                os.remove(os.path.join(unzip_px_server_package_path, 'grpc_node.node'))
+                os.remove(grpc_file_path)
             except OSError as e:
                 if e.errno != errno.ENOENT:
                     raise
 
             try:
-                os.remove(os.path.join(unzip_px_server_package_path, 'px'))
+                os.remove(px_server_executable_file_path)
             except OSError as e:
                 if e.errno != errno.ENOENT:
                     raise
@@ -46,9 +57,9 @@ class Server:
             print('Downloading px server package...')
             file_name, header = urllib.request.urlretrieve(px_server_package_download_url)
 
-            print('Extracting px server package from {} to {}'.format(file_name, unzip_px_server_package_path))
+            print('Extracting px server package from {} to {}'.format(file_name, px_server_package_path))
             with zipfile.ZipFile(file_name, 'r') as zip_ref:
-                zip_ref.extractall(unzip_px_server_package_path)
+                zip_ref.extractall(px_server_package_path)
 
             try:
                 print('Deleting px server package...')
@@ -56,8 +67,21 @@ class Server:
             except OSError:
                 pass
 
-            os.chmod(os.path.join(unzip_px_server_package_path, 'grpc_node.node'), 0o755)
-            os.chmod(os.path.join(unzip_px_server_package_path, 'px'), 0o755)
+            os.chmod(grpc_file_path, 0o755)
+            os.chmod(px_server_executable_file_path, 0o755)
+
+    def get_px_server_package_download_url(self, os_name=None, version=None):
+        if os_name is None:
+            os_name = self.os_name
+
+        if os_name == 'Windows':
+            return self.windows_px_server_package_download_url
+        elif os_name == 'Linux':
+            return self.linux_px_server_package_download_url
+        elif os_name == 'Darwin':
+            return self.mac_px_server_package_download_url
+        else:
+            raise Exception('Unsupported OS: {}'.format(os_name))
 
     def stop(self):
         print('Stopping px server...')
@@ -67,5 +91,6 @@ class Server:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(("",0))
         self.port = s.getsockname()[1]
-        self.thread = threading.Thread(target=subprocess.call, args=[[self.px_server_executable_file_path, str(self.port)]], daemon=True)
+        px_server_executable_file_path = os.path.join(self.px_server_package_path, 'px')
+        self.thread = threading.Thread(target=subprocess.call, args=[[px_server_executable_file_path, str(self.port)]], daemon=True)
         self.thread.start()
