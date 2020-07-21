761bd971fa15fdc4d6e6d83a67cf020d59fe1c68
Anh Le <anhqle@kms-technology.com>
2020-07-21 15:17:13 +0700

    add reload action python

--- python/px_client/client.py
+++ python/px_client/client.py
@@ -82,6 +82,10 @@ class Client:
         actions = px.Action(selectAction=px.SelectAction(selector=selector, values=values))
         self.do_request(actions)
 
+    def reload(self):
+        actions = px.Action(reloadAction=px.ReloadAction())
+        self.do_request(actions)
+
     def type(self, selector, text):
         actions = px.Action(typeAction=px.TypeAction(selector=selector, text=text))
         self.do_request(actions)
